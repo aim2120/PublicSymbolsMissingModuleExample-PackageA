f5b94b1f87de5ad311943567c1fd0af8e6738bf9
Annalise Mariottini <annalise.mariottini@columbia.edu>
2025-05-06 12:16:10 -0400

    remove dynamic from package product

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PublicSymbolsMissingModuleExample-PackageA",
-            type: .dynamic,
             targets: ["PublicSymbolsMissingModuleExample-PackageA"]),
     ],
     dependencies: [
